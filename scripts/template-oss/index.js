1c5c9ab8afe1fa0a92fdd14bd08942db125aa1d2
Luke Karrys <luke@lukekarrys.com>
2022-09-07 10:01:53 -0700

    wip

---
+++ scripts/template-oss/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  rootRepo: {
+    add: {
+      '.github/ISSUE_TEMPLATE/config.yml': false,
+      '.github/ISSUE_TEMPLATE/bug.yml': false,
+      '.github/dependabot.yml': false,
+      '.github/workflows/ci.yml': 'ci.yml',
+      '.github/workflows/release.yml': 'release.yml',
+    },
+  },
+  lockfile: true,
+  npm: '.',
+  defaultBranch: 'latest',
+  distPaths: [
+    'index.js',
+    'docs/content/**/*.md',
+    'docs/output/**/*.html',
+    'man',
+  ],
+  allowPaths: [
+    '/node_modules/',
+    '/index.js',
+    '/Makefile',
+    '/make.bat',
+    '/DEPENDENCIES.md',
+    '/CONTRIBUTING.md',
+    '/configure',
+    '/changelogs/',
+    '/AUTHORS',
+    '/.mailmap',
+    '/.licensee.json',
+    '/.gitattributes',
+  ],
+  ignorePaths: [
+    '/node_modules/.bin/',
+    '/node_modules/.cache/',
+  ],
+}
